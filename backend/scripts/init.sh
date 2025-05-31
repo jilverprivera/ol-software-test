53cf5de8c595b4ddc4e4e58e503884db0b302b87
Jilver Pacheco <jilverrivera1@gmail.com>
2025-05-31 17:04:01 -0500

    feat: add docker support

---
+++ backend/scripts/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Wait for database to be ready
+echo "Waiting for database to be ready..."
+/app/wait-for-it.sh postgres:5432 -t 60
+
+# Run database migrations
+echo "Running database migrations..."
+npx prisma migrate deploy
+
+# Run database seed
+echo "Running database seed..."
+npx prisma db seed
+
+# Start the application
+echo "Starting the application..."
+yarn start:prod 
